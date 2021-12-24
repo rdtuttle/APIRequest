2d131d86696327db90adffa03036710a5a26da97
Russ Tuttle <russelltuttle1@gmail.com>
2021-12-24 14:52:04 -0500

    display user data

--- student-files/js/scripts.js
+++ student-files/js/scripts.js
@@ -2,26 +2,28 @@ const gallery = document.getElementById("gallery")
 
 
 //get user data
-fetch("https://randomuser.me/api/?results=30")
+fetch("https://randomuser.me/api/?results=12&?nat=us")
   .then(response => response.json())
   .then(data => generateCard(data.results))
+  .catch (error => console.log(error))
 
   //helper function to display data
   function generateCard(data) {
     const cards = data.map(card => `
     <div class="card">
     <div class="card-img-container">
-        <img class="card-img" src="https://placehold.it/90x90" alt="profile picture">
+        <img class="card-img" src=${card.picture.thumbnail} alt="profile picture">
     </div>
     <div class="card-info-container">
-        <h3 id="name" class="card-name cap">first last</h3>
-        <p class="card-text">email</p>
-        <p class="card-text cap">city, state</p>
+        <h3 id="name" class="card-name cap">${card.name.first} ${card.name.last}</h3>
+        <p class="card-text">${card.email}</p>
+        <p class="card-text cap">${card.location.city}, ${card.location.state}</p>
     </div>
-</div>)
-`);
-gallery.innerHTML = cards
-  }
+</div>
+`)
+.join("");
+gallery.insertAdjacentHTML("beforeend", cards)
+}
   
   
   
