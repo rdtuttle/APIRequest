e11d370dc6faec18f1566366638af724b5ebea94
Russ Tuttle <russelltuttle1@gmail.com>
2021-12-25 01:19:32 -0500

    modal container update

--- student-files/js/scripts.js
+++ student-files/js/scripts.js
@@ -1,4 +1,8 @@
 const gallery = document.getElementById("gallery")
+const card = document.querySelector(".card")
+
+
+
 
 
 //get user data
@@ -25,6 +29,39 @@ fetch("https://randomuser.me/api/?results=12&?nat=us")
 gallery.insertAdjacentHTML("beforeend", cards)
 }
   
+
+
+//display modal
+const body = document.querySelector("body")
+
+function  generateModal(index) {
+const modal = 
+`
+<div class="modal-container">
+<div class="modal">
+    <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+    <div class="modal-info-container">
+        <img class="modal-img" src=${index.picture} alt="profile picture">
+        <h3 id="name" class="modal-name cap">name</h3>
+        <p class="modal-text">email</p>
+        <p class="modal-text cap">city</p>
+        <hr>
+        <p class="modal-text">(555) 555-5555</p>
+        <p class="modal-text">123 Portland Ave., Portland, OR 97204</p>
+        <p class="modal-text">Birthday: 10/21/2015</p>
+    </div>
+</div>;
+`
+//Add the modal HTML inside of body.
+body.insertAdjacentHTML('beforeend', modal);
+//listener to close modal button
+const closeButton = document.getElementById("modal-close-btn")
+const modalcontainer = document.querySelector('.modal-container')
+closeButton.addEventListener("click", () => {
+  modalcontainer.remove();
+})
+
+}
   
-  
-  
+gallery.addEventListener("click", generateModal)
+
